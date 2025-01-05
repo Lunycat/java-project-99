9f603cc2ff256ddc3abcc839cd9a53831c39bc19
Artem <lunycat@mail.ru>
2025-01-05 23:06:32 +0300

    add checkstyle, jacoco

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     application
+    jacoco
+    checkstyle
     id("io.freefair.lombok") version "8.6"
     id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.3"
@@ -13,13 +15,27 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-devtools")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-devtools")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-configuration-processor")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+
+    implementation("org.openapitools:jackson-databind-nullable:0.2.6")
 
     implementation("org.mapstruct:mapstruct:1.6.0.Beta1")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.6.0.Beta1")
+
+    implementation("org.instancio:instancio-junit:3.6.0")
+    implementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
+    implementation("net.datafaker:datafaker:2.0.2")
+
+    runtimeOnly("com.h2database:h2:2.2.224")
 
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
@@ -28,6 +44,8 @@ dependencies {
 }
 
 application { mainClass.set("io.hexlet.blog.Application") }
+
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
 
 tasks.withType<Test> {
     useJUnitPlatform()
--- app/src/main/java/hexlet/code/model/User.java
+++ app/src/main/java/hexlet/code/model/User.java
@@ -1,9 +1,28 @@
 package hexlet.code.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.EqualsAndHashCode;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "users")
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -12,6 +31,23 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
-    
+
+    @EqualsAndHashCode.Include
+    private String firstName;
+
+    @EqualsAndHashCode.Include
+    private String lastName;
+
+    @EqualsAndHashCode.Include
+    private String email;
+
+    private String password;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updateAt;
 }
