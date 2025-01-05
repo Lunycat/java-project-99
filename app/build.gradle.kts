d48f23728046b3155b8e58b3fd66b8abe3a86055
Artem <lunycat@mail.ru>
2025-01-05 16:59:08 +0300

    replace add

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
 	java
-	war
 	id("org.springframework.boot") version "3.4.1"
 	id("io.spring.dependency-management") version "1.1.7"
 }
@@ -19,9 +18,9 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-
-	providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
+	implementation("org.springframework.boot:spring-boot-devtools")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
